6bfaeaf6f682b71763a3b7491a7bc07ca29ceab1
mscheide <mscheide@gmail.com>
2025-03-15 17:23:24 -0300

    add: new server with 2 routes: sum and sub

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,35 @@ use calc_near_x::calc_with_1;
 use tide::Request;
 
 #[async_std::main]
-async fn main() -> tide::Result<()> {
-    println!("⚙️ Iniciando Server...");
 
-    let mut app = tide::new();
+async fn main()-> tide::Result<()> {
+    let mut app = tide::new(); 
 
-    app.at("/").get(|_| async move { Ok("👋 Hello Tide!") });
+    app.at("/").get(|_| async { Ok("Hello Tide! 😃") });
     app.at("/sum/:x/:y").get(sum);
     app.at("/sub/:x/:y").get(sub);
 
-    println!("✅ Servidor online");
-    println!("🔗 Access em: http://0.0.0.0:3003");
+    println!("Server running on port 3003");
 
-    app.listen("0.0.0.0:3003").await?;
+    app.listen("127.0.0.1:3003").await?;
+
     Ok(())
 }
 
 async fn sum(req: Request<()>) -> tide::Result {
-    let x = req.param("x").unwrap_or("0").parse().unwrap_or(0);
-    let y = req.param("y").unwrap_or("0").parse().unwrap_or(0);
-
-    let z = calc_with_1::sum_plus_one(x, y);
-
-    Ok(format!("Resultado: {}", z).into())
+    let x= req.param("x").unwrap_or("0").parse().unwrap_or(0);
+    let y= req.param("y").unwrap_or("0").parse().unwrap_or(0);
+    
+    let z=calc_with_1::sum_plus_one(x, y);
+    
+    Ok(format!("Result: {}", z).into())
 }
 
 async fn sub(req: Request<()>) -> tide::Result {
-    let x = req.param("x").unwrap_or("0").parse().unwrap_or(0);
-    let y = req.param("y").unwrap_or("0").parse().unwrap_or(0);
+    let x= req.param("x").unwrap_or("0").parse().unwrap_or(0);
+    let y= req.param("y").unwrap_or("0").parse().unwrap_or(0);
 
-    let z = calc_with_1::sub_less_one(x, y);
-
-    Ok(format!("Resultado: {}", z).into())
+    let z=calc_with_1::sub_less_one(x, y);
+    
+    Ok(format!("Result: {}", z).into())
 }
